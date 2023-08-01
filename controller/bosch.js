9ad7d0078dacf0edab0ebfb3f6324a78a187634e
sheetaldhingra <sheetaldhingra4@gmail.com>
2023-08-01 20:42:11 +0530

    model changes

--- controller/bosch.js
+++ controller/bosch.js
@@ -1,4 +1,4 @@
-const Product = require("../models/product");
+const Product = require("../models/bosch");
 const connectDB = require("../db/connect");
 const ProductJson = require("../products.json");
 
